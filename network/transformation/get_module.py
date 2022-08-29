725b5aa153f94a2c5ac7c9cae942efeb2db983b2
Houang869 <48252530+Houangnt@users.noreply.github.com>
2022-08-29 11:08:46 +0700

    Update get_module.py

--- network/transformation/get_module.py
+++ network/transformation/get_module.py
@@ -1,4 +1,6 @@
 from .spin_transform import *
 from .tps_transform import *
 
-list_modules = ['spin', 'tps', 'none']
+#list_modules = ['spin', 'tps', 'none']
+
+list_modules = ['spin', 'tps','spin_w_tps','none']
